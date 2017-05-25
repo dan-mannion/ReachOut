721f78947e0d3c0e4723cfd9826529d891c1b7a3
Daniel Mannion <danjohnmannion@gmail.com>
2017-05-25 11:56:11 +0100

    Adds Capacitive Touch Demo

---
+++ CapacitiveSensorDemo.ino
@@ -0,0 +1,32 @@
+#include <CapacitiveSensor.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10M between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ * Edited Dan Mannion 2017
+ * Simplified to single sensor for demonstration.
+ */
+
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+
+void setup()                    
+{
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(30);
+    // check on performance in milliseconds
+    Serial.print("\n");                    // tab character for debug windown spacing
+
+    Serial.print(total1);                  // print sensor output 1
+
+    delay(250);                             // arbitrary delay to limit data to serial port 
+}
